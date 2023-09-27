6fa049e475b40a5f75f9ba503c1c7597cb0c4b03
ibnomarmahmoud <mahmoudoosman007@gmail.com>
2023-09-27 13:06:06 +0000

    #9 S3 Creation and Destroy using TF AWS Provider

--- main.tf
+++ main.tf
@@ -5,18 +5,38 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
   }
+}
+
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
   # Configuration options
 }
 resource "random_string" "bucket_name" {
-  length   = 20
+  length   = 28
   special  = false
   lower    = true
+  upper = false
 }
 
 output "random_bucket_name" {
   value = random_string.bucket_name.result
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
