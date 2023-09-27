b04e952a4e09f617b6607ba18f41de987cee6782
ibnomarmahmoud <mahmoudoosman007@gmail.com>
2023-09-27 11:40:17 +0000

    #7 Creating Random bucket name via Terraform random provider

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
 
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+resource "random_string" "bucket_name" {
+  length   = 20
+  special  = false
+  lower    = true
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
