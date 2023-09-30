62a22e270ee9fb74a88af5e3761481179d1b1156
ibnomarmahmoud <mahmoudoosman007@gmail.com>
2023-09-30 11:57:53 +0000

    #17 Root Restruction

---
+++ Provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  
+#  cloud {
+#    organization = "TerraTown-Mahmoud-2023"
+#    workspaces {
+#      name = "TerraTown-1"
+#    }
+#   }
+    required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
+  }
+}
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
--- main.tf
+++ main.tf
@@ -1,45 +1,9 @@
-
-terraform {
-  
-  cloud {
-    organization = "TerraTown-Mahmoud-2023"
-    workspaces {
-      name = "TerraTown-1"
-    }
-  
-    }
-    required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.18.1"
-    }
-  }
-}
-
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
 resource "random_string" "bucket_name" {
   length   = 28
   special  = false
   lower    = true
   upper = false
 }
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
-
-
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
