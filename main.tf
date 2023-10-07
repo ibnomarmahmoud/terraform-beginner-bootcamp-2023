2e02780fcd35a0ea0c37644b4550fd2f14d95f3b
ibnomarmahmoud <125532497+ibnomarmahmoud@users.noreply.github.com>
2023-10-07 18:59:35 +0300

    Deploy on TerraTown (#36)

--- main.tf
+++ main.tf
@@ -23,30 +23,29 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = "https://terratowns.cloud/api"
+  user_uuid="47a922c1-3dd0-4147-999f-d8d24e4a78e6" 
+  token="45755add-2b01-4cbd-8fd0-029d2c372631"
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
-resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!XY"
+resource "terratowns_home" "home_mahmoud1" {
+  name = "Terraform"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+Terraform is an infrastructure-as-code software tool created by HashiCorp. 
+Users define and provide data center infrastructure using a declarative configuration language known as HashiCorp Configuration Language (HCL), or optionally JSON
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdq3gz1.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
